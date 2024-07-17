a264a92047cfebb49c39965ddf8f5e23d4996020
Vinay Kumar <raparthivinay99@gmail.com>
2024-07-17 14:14:30 +0530

    first commit

---
+++ app/src/main/java/com/vinay/recordkeeper/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.vinay.recordkeeper
+
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.vinay.recordkeeper.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
+        val frame: FrameLayout = binding.frameContent
+        val bottomNav: BottomNavigationView = binding.bottomNav
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+
+    }
+}
